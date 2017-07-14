85ce8867e0ca4c01a457fe81dc786e4c4f5b0b5a
Hamon Riazy <hamon.riazy@gmail.com>
2017-07-14 13:01:52 +0100

    Initial commit

---
+++ SomeClass.swift
@@ -0,0 +1,19 @@
+//
+//  SomeClass.swift
+//  flickrpickr-sdk
+//
+//  Created by Hamon Riazy on 14/07/2017.
+//  Copyright © 2017 Hamon Riazy. All rights reserved.
+//
+
+import Foundation
+
+
+
+class Whatever {
+    
+    func doSomething() {
+        
+    }
+    
+}
---
+++ flickrpickr-sdk/flickrpickr-sdk.h
@@ -0,0 +1,19 @@
+//
+//  flickrpickr-sdk.h
+//  flickrpickr-sdk
+//
+//  Created by Hamon Riazy on 14/07/2017.
+//  Copyright © 2017 Hamon Riazy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for flickrpickr-sdk.
+FOUNDATION_EXPORT double flickrpickr_sdkVersionNumber;
+
+//! Project version string for flickrpickr-sdk.
+FOUNDATION_EXPORT const unsigned char flickrpickr_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <flickrpickr_sdk/PublicHeader.h>
+
+
